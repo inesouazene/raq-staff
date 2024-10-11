f07d0e3d50e94e538556099f9a4cfa9e90aa9f21
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-10-11 23:20:56 +0200

    Add jsx extension to App.js frontend file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+// App.jsx
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "react-big-schedule/dist/css/style.css";
+import HomePage from './components/HomePage';
+import Employees from './components/Employees';
+import EmployeeDetails from './components/EmployeeDetails';
+import Navigation from './components/Navigation';
+import Schedule from './components/Schedule';
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <h1 className="app-title">Employee Planning Application</h1>
+        <Navigation />
+        <main className="app-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/employees/:id" element={<EmployeeDetails />} />
+            <Route path="/schedules" element={<Schedule />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
