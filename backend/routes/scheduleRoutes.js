f07c1753e1848ef4ad1fdfedef8ee11c52833513
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-10-16 15:48:33 +0200

    Update scheduleRoutes to add routes for : - add a task - update a task - delete a task

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -10,4 +10,13 @@ router.get('/employees', scheduleController.getAllEmployees);
 // Récupérer les tâches d'une semaine spécifique
 router.get('/tasks/week', scheduleController.getTasksByWeek);
 
+// Route pour ajouter une nouvelle tâche
+router.post('/tasks/add', scheduleController.addTask);
+
+// Route pour supprimer une tâche
+router.delete('/tasks/:id', scheduleController.deleteTask);
+
+// Route pour modifier une tâche
+router.put('/tasks/:id', scheduleController.updateTask);
+
 module.exports = router;
