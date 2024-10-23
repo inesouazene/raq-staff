2a42130d0d91d0981ef770f3e81f54fb22d6d362
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-10-23 18:24:40 +0200

    Update App.jsx : integration of DashboardLayout component and ThemeProvider for light mode/dark mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,40 @@
-// App.jsx
+import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import "react-big-schedule/dist/css/style.css";
-import HomePage from './components/HomePage';
-import Employees from './components/Employees';
-import EmployeeDetails from './components/EmployeeDetails';
-import Navigation from './components/Navigation';
-import Schedule from './components/Schedule';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import DashboardLayout from './components/DashboardLayout';
+import { lazy, Suspense } from 'react';
+import { lightTheme, darkTheme } from './themes/theme';
+
+// Importing the lazy-loaded components
+const HomePage = lazy(() => import('./components/HomePage'));
+const Employees = lazy(() => import('./components/Employees'));
+const EmployeeDetails = lazy(() => import('./components/EmployeeDetails'));
+const Schedule = lazy(() => import('./components/Schedule'));
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <Router>
-      <div className="app-container">
-        <h1 className="app-title">Raq&Staff</h1>
-        <Navigation />
-        <main className="app-content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/employees" element={<Employees />} />
-            <Route path="/employees/:id" element={<EmployeeDetails />} />
-            <Route path="/schedules" element={<Schedule />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <Router>
+        <DashboardLayout isDarkMode={isDarkMode} toggleTheme={toggleTheme}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/employees/:id" element={<EmployeeDetails />} />
+              <Route path="/schedules" element={<Schedule />} />
+            </Routes>
+          </Suspense>
+        </DashboardLayout>
+      </Router>
+    </ThemeProvider>
   );
 }
 
