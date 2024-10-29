3bddf0d923fd30dfdbce8c4212434c612b1fecec
inesouazene <142597545+inesouazene@users.noreply.github.com>
2024-10-29 12:33:56 +0100

    Update of Schedule Controller : - add method to get tasks types - update of addTask method to add a tasks to only one or  multiple employees

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -29,21 +29,29 @@ const scheduleModel = require('../models/schedule');
 		// Méthode pour ajouter une tâche
 		addTask: async (req, res) => {
 			try {
-				const { date_tache, heure_debut, heure_fin, id_salarie, id_type_tache, description } = req.body;
+			  const { date_tache, heure_debut, heure_fin, id_salarie, id_type_tache, pause } = req.body;
 
-				// Ajout de la tâche via le modèle
-				const newTask = await scheduleModel.addTask({
-					date_tache,
-					heure_debut,
-					heure_fin,
-					id_salarie,
-					id_type_tache,
-					description
-				});
+			  // Si id_salarie n'est pas un tableau, retournez une erreur
+			  if (!Array.isArray(id_salarie)) {
+				return res.status(400).json({ error: 'Le champ id_salarie doit être un tableau.' });
+			  }
 
-				res.status(201).json({ message: 'Tâche ajoutée avec succès!', task: newTask });
+			  // Crée un tableau de tâches pour chaque salarié sélectionné
+			  const taskDataArray = id_salarie.map(salarieId => ({
+				date_tache,
+				heure_debut,
+				heure_fin,
+				id_salarie: salarieId,
+				id_type_tache,
+				pause
+			  }));
+
+			  // Appelle le modèle pour ajouter toutes les tâches en une seule fois
+			  const tasks = await scheduleModel.addTask(taskDataArray);
+
+			  res.status(201).json({ message: 'Tâches ajoutées avec succès!', tasks });
 			} catch (error) {
-				res.status(500).json({ error: error.message });
+			  res.status(500).json({ error: error.message });
 			}
 		},
 		// Méthode pour supprimer une tâche
@@ -66,7 +74,7 @@ const scheduleModel = require('../models/schedule');
 		updateTask: async (req, res) => {
 			try {
 				const { id } = req.params;
-				const { date_tache, heure_debut, heure_fin, id_salarie, id_type_tache, description } = req.body;
+				const { date_tache, heure_debut, heure_fin, id_salarie, id_type_tache, pause } = req.body;
 
 				// Mise à jour de la tâche via le modèle
 				const updatedTask = await scheduleModel.updateTask(id, {
@@ -75,7 +83,7 @@ const scheduleModel = require('../models/schedule');
 					heure_fin,
 					id_salarie,
 					id_type_tache,
-					description
+					pause
 				});
 
 				if (updatedTask) {
@@ -86,7 +94,15 @@ const scheduleModel = require('../models/schedule');
 			} catch (error) {
 				res.status(500).json({ error: error.message });
 			}
-		}
+		},
+		getTaskTypes: async (req, res) => {
+			try {
+			  const taskTypes = await scheduleModel.getTaskTypes();
+			  res.json(taskTypes);
+			} catch (error) {
+			  res.status(500).json({ error: error.message });
+			}
+		},
 };
 
 module.exports = scheduleController;
